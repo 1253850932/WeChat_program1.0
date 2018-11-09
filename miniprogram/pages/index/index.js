a3f238bf14e61140e4334f5b34285d35abef54a1
iscream <210@qq.com>
2018-11-09 10:23:44 +0800

    ''

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,14 @@
+//index.js  
+//获取应用实例  
+var app = getApp()
+Page({
+  data: {
+    movies: [
+      { url: 'https://p.qpic.cn/qqconadmin/0/44512bc46be24d08be4cd170b4a91338/0' },
+      { url: 'https://p.qpic.cn/qqconadmin/0/01da4f9f8d97452d8cdcc1596954656e/0' },
+      { url: 'https://p.qpic.cn/qqconadmin/0/e9f1e222e6174115b404c431950ea899/0' }
+    ]
+  },
+  onLoad: function () {
+  }
+})  
